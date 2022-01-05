14c404713847e702617e0057cc40156603f00a2e
Stefan Crooijmans <36646231+stefcroo@users.noreply.github.com>
2022-01-05 18:05:15 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+def create_video():
+    image_folder = os.getcwd()
+    video_name = 'your_file.avi'
+    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+    images.sort()
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(video_name, 0, 8, (width,height))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+    cv2.destroyAllWindows()
+    video.release()
+create_video()
